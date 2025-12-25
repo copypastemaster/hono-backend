6ba77a7c7c523e98991281d9a4b8ff56f1241aac
Enoch <enoch.binas@gmail.com>
2025-12-25 22:20:14 +0800

    chore: change migrations location

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
     out: "./drizzle",
-    schema: [], // List your schemas
+    schema: "./src/db/migrations",
     dialect: "postgresql",
     dbCredentials: {
         url: process.env.DATABASE_URL!,
